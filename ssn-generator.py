bc26161047ec0029999ae0877949c58412bf73c8
Santeri <santeri.sydanmetsa@pm.me>
2022-06-06 15:03:59 +0300

    Python: updated random_date function
    The old random_date function failed if the date was earlier than the UNIX epoch (1st Jan 1970). The new approach uses the timedelta object to add days to the min_date. Is this approach better in other languages (JS for now), too?

--- ssn-generator.py
+++ ssn-generator.py
@@ -1,102 +1,89 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from random import randint
 
 
 def new_date_zero_time(d: datetime = None) -> datetime:
-  if d is None:
-    d = datetime.now()
-  return datetime(d.year, d.month, d.day, 0, 0, 0, 0)
+    if d is None:
+        d = datetime.now()
+    return datetime(d.year, d.month, d.day, 0, 0, 0, 0)
 
 
 def new_date_delta(d: datetime, year_delta = 0, month_delta = 0, day_delta = 0) -> datetime:
-  d = datetime(d.year + year_delta, d.month + month_delta, d.day + day_delta)
-  return d
+    d = datetime(d.year + year_delta, d.month + month_delta, d.day + day_delta)
+    return d
 
 
 def random_date(min_date: datetime, max_date: datetime) -> datetime:
-  timestamp = randint(int(min_date.timestamp()), int(max_date.timestamp()))
-  return new_date_zero_time(datetime.fromtimestamp(timestamp))
+    delta = max_date - min_date
+    days = randint(0, delta.days)
+    return min_date + timedelta(days=days)
 
 
 def random_date_from_age(years: int) -> datetime:
-  min_date = new_date_delta(new_date_zero_time(), -(years + 1), 0, +1)
-  max_date = new_date_delta(new_date_zero_time(), -years, 0, 0)
-  return random_date(min_date, max_date)
+    min_date = new_date_delta(new_date_zero_time(), -(years + 1), 0, +1)
+    max_date = new_date_delta(new_date_zero_time(), -years, 0, 0)
+    return random_date(min_date, max_date)
 
 
 def random_date_from_age_range(min_years: int, max_years: int) -> datetime:
-  years = randint(min_years, max_years)
-  return random_date_from_age(years)
+    years = randint(min_years, max_years)
+    return random_date_from_age(years)
 
 
 def parse_date_short_format(d: datetime) -> str:
-  return d.strftime("%d%m%y")
+    return d.strftime("%d%m%y")
 
 
 def get_delimiter(d: datetime) -> str:
-  delimiters = {20: "A", 19: "-", 18: "+"}
-  century = d.year // 100
-  return delimiters[century]
+    delimiters = {20: "A", 19: "-", 18: "+"}
+    century = d.year // 100
+    return delimiters[century]
 
 
 def random_identity_number() -> str:
-  n = randint(2, 899)
-  return f"{n:03}"
+    n = randint(2, 899)
+    return f"{n:03}"
 
 
 def get_check_symbol(nine_digit_number: int) -> str:
-  check_symbols = "0123456789ABCDEFHJKLMNPRSTUVWXY"
-  return check_symbols[nine_digit_number % 31]
+    check_symbols = "0123456789ABCDEFHJKLMNPRSTUVWXY"
+    return check_symbols[nine_digit_number % 31]
 
 
 def generate_ssn_from_date(d: datetime) -> str:
-  date_of_birth = parse_date_short_format(d)
-  delimiter = get_delimiter(d)
-  identity_number = random_identity_number()
-  nine_digit_number = int(date_of_birth + identity_number)
-  check_symbol = get_check_symbol(nine_digit_number)
-  return (
-    date_of_birth
-    + delimiter
-    + identity_number
-    + check_symbol
-  )
+    date_of_birth = parse_date_short_format(d)
+    delimiter = get_delimiter(d)
+    identity_number = random_identity_number()
+    nine_digit_number = int(date_of_birth + identity_number)
+    check_symbol = get_check_symbol(nine_digit_number)
+    return (
+      date_of_birth
+      + delimiter
+      + identity_number
+      + check_symbol
+    )
 
 
 def generate_ssn_age_exactly(years: int) -> str:
-  d = new_date_delta(new_date_zero_time(), -years, 0, 0)
-  return generate_ssn_from_date(d)
+    d = new_date_delta(new_date_zero_time(), -years, 0, 0)
+    return generate_ssn_from_date(d)
 
 
 def generate_ssn_age_one_day_shy_from(years: int) -> str:
-  d = new_date_delta(new_date_zero_time(), -years, 0, +1)
-  return generate_ssn_from_date(d)
+    d = new_date_delta(new_date_zero_time(), -years, 0, +1)
+    return generate_ssn_from_date(d)
 
 
 def generate_ssn_age_one_day_over(years: int) -> str:
-  d = new_date_delta(new_date_zero_time(), -years, 0, -1)
-  return generate_ssn_from_date(d)
+    d = new_date_delta(new_date_zero_time(), -years, 0, -1)
+    return generate_ssn_from_date(d)
 
 
 def generate_ssn_age_between(min_years: int, max_years: int) -> str:
-  d = random_date_from_age_range(min_years, max_years)
-  return generate_ssn_from_date(d)
+    d = random_date_from_age_range(min_years, max_years)
+    return generate_ssn_from_date(d)
 
 
-class RandomSSN:
-
-  @staticmethod
-  def exactly(years: int) -> str:
-    return generate_ssn_age_exactly(years)
-
-  @staticmethod
-  def almost(years: int) -> str:
-    return generate_ssn_age_one_day_shy_from(years)
-
-  @staticmethod
-  def barely(years: int) -> str:
-    return generate_ssn_age_one_day_over(years)
-
-  @staticmethod
-  def between(min_years: int, max_years: int) -> str:
-    return generate_ssn_age_between(min_years, max_years)
+def generate_ssn_eighteen_to_sixtyfive():
+    d = random_date_from_age_range(18, 121)
+    return generate_ssn_from_date(d)
